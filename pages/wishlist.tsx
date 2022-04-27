d4f6eb863bc5f0a20f5e226ccb52dd4e98bbf7b4
fredwills9224 <willsf9224@gmail.com>
2022-04-27 13:14:35 -0400

    empty cart + whishlist upd8

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -24,7 +24,7 @@ export default function Wishlist(){
                                 Your wishlist is empty
                             </h2>
                             <p className='text-accents-6 px-10 text-center pt-2'>
-                                Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
+                                Please continue shopping.
                             </p>
                         </div>
                     ):(
